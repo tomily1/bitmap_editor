796b02dd90fce6c192525112c77a77f2d6be4755
Isreal <itomilayo@gmail.com>
2020-02-28 15:16:14 +0100

    write specs to describe the bitmap editor's behavior

---
+++ spec/lib/bitmap_editor_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe BitmapEditor do
+  let(:invalid_extension) { fixture('invalid_extension.csv')}
+  let(:invalid_path) { fixture('invalid_path.txt') }
+  let(:invalid_command) { fixture('invalid_command.txt') }
+  let(:valid_file) { fixture('valid_file.txt') }
+  let(:empty_file) { fixture('empty_file.txt') }
+
+  describe '#initialize' do
+    it 'creates a new instance of the bitmap editor'  do
+      expect(described_class.new(valid_file)).to be_truthy
+    end
+  end
+
+  describe '#run_commands' do
+    context 'returns validations error' do
+      it 'when file extension is invalid' do
+        expect do
+          described_class.new(invalid_extension).run_commands
+        end.to output(/Invalid File format/).to_stdout
+      end
+
+      it 'when file path is invalid' do
+        expect do
+          described_class.new(invalid_path).run_commands
+        end.to output(/File does not exist/).to_stdout
+      end
+
+      it 'when non supported command directive is used' do
+        expect do
+          described_class.new(invalid_path).run_commands
+        end.to output(/Invalid command(s)/).to_stdout
+      end
+    end
+
+    context 'returns valid output' do
+
+      it 'when file and commands are valid' do
+        expect do
+          described_class.new(valid_file).run_commands
+        end.to output("OOOOO\nOOZZZ\nAWOOO\nOWOOO\nOWOOO\nOWOOO").to_stdout
+
+        expect do
+          described_class.new(empty_file).run_commands
+        end.to output("").to_stdout
+      end
+    end
+  end
+end
