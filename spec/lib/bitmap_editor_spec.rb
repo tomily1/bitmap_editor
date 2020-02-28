ca61b8b0fa9f19ab291cb73c380b2dce0a1ef196
Isreal <itomilayo@gmail.com>
2020-02-28 15:31:48 +0100

    add more specs to describe behavior

--- spec/lib/bitmap_editor_spec.rb
+++ spec/lib/bitmap_editor_spec.rb
@@ -6,6 +6,8 @@ describe BitmapEditor do
   let(:invalid_command) { fixture('invalid_command.txt') }
   let(:valid_file) { fixture('valid_file.txt') }
   let(:empty_file) { fixture('empty_file.txt') }
+  let(:i_file) { fixture('i.txt') }
+  let(:c_file) { fixture('c.txt') }
 
   describe '#initialize' do
     it 'creates a new instance of the bitmap editor'  do
@@ -35,7 +37,6 @@ describe BitmapEditor do
     end
 
     context 'returns valid output' do
-
       it 'when file and commands are valid' do
         expect do
           described_class.new(valid_file).run_commands
@@ -44,6 +45,16 @@ describe BitmapEditor do
         expect do
           described_class.new(empty_file).run_commands
         end.to output("").to_stdout
+      end
+    end
+
+    context 'commands' do
+      it 'creates M x N pixels when sent \'I 4 4\' directive' do
+        expect(described_class.new(i_file).run_commands).to eq("OOOO\nOOOO\nOOOO\nOOOO\n")
+      end
+
+      it 'clears the M x N pixels when sent \'C\' directive' do
+        expect(described_class.new(c_file).run_commands).to eq("OOOO\nOOOO\nOOOO\nOOOO\n")
       end
     end
   end
